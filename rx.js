299c3350f72775418f493a119ee8fd1ffd2a04e1
rmfhsep <rmfhsep@gmail.com>
2024-02-22 22:58:10 +0900

    obsrvable

--- rx.js
+++ rx.js
@@ -1,15 +1,50 @@
-const { of, from, range, generate } = rxjs;
+const Rx = require("rxjs");
+const { take } = require("rxjs/operators");
+// 어레이로부터 만들기 (frOm)
+// const deliveries = ["delivery1", "delivery2", "delivery3"];
 
-const obs1$ = of(1, 2, 3, 4, 5);
-const obs2$ = from([6, 7, 8, 9, 10]);
-const obs3$ = range(11, 5);
-const obs4$ = generate(
-  15,
-  (x) => x < 30,
-  (x) => x + 2
-);
+// const stream = Rx.from(deliveries);
+// // stream.subscribe({
+// //   next: (data) => {
+// //     console.log(data);
+// //   },
+// //   error: (err) => {
+// //     console.log(err);
+// //   },
+// //   complete: () => {
+// //     console.log("completed");
+// //   },
+// // });
 
-obs1$.subscribe((item) => console.log(`of: ${item}`));
-//obs2$.subscribe(item => console.log(`from: ${item}`))
-//obs3$.subscribe(item => console.log(`range: ${item}`))
-//obs4$.subscribe(item => console.log(`generate: ${item}`))
+// // 프로미스로부터 만들기
+// function makePromise() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("deliev");
+//     }, 3000);
+//   });
+// }
+
+// Rx.from(makePromise()).subscribe({
+//   next: (data) => {
+//     console.log(data);
+//   },
+// });
+
+// ----------------------
+// const stream = Rx.interval(1000);
+
+// stream.pipe(take(10)).subscribe({
+//   next: (data) => {
+//     console.log(data);
+//   },
+//   complete: () => {},
+// });
+
+const stream = Rx.timer(3000, 1000);
+
+stream.pipe(take(10)).subscribe({
+  next: (data) => {
+    console.log(data);
+  },
+});
